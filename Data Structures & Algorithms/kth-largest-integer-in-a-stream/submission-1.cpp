d657a997a323b1e9541bb02beff7cc7db4f8b9be
Chahat Chopra <chahat.chopra1556@gmail.com>
2026-08-23 23:51:16 +0530

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.cpp
@@ -0,0 +1,23 @@
+class KthLargest {
+public:
+    int k;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+
+        for (int val : nums) {
+            add(val);
+        }
+    }
+
+    int add(int val) {
+        minHeap.push(val);
+
+        if (minHeap.size() > k) {
+            minHeap.pop();
+        }
+
+        return minHeap.top();
+    }
+};
